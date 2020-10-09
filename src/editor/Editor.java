2dea7a14d5595b221d10346e71ab8960a46871de
ematarkus <ematarkus@example.com>
2020-10-09 03:45:12 +0200

    unused code

--- src/editor/Editor.java
+++ src/editor/Editor.java
@@ -30,7 +30,6 @@ import java.awt.*;
 import java.awt.datatransfer.*;
 import java.awt.event.*;
 import java.awt.font.TextAttribute;
-import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
 
